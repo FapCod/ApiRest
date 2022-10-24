37877db216d59e2fc5f8ef7efe3f0ce6e168b654
Fapcod <frankantoniopiato@gmail.com>
2022-10-24 17:23:03 -0500

    apirest del reto 3

---
+++ ApiRest/Controllers/TareaController.cs
@@ -0,0 +1,89 @@
+﻿using ApiRest.Datos;
+using ApiRest.Models;
+using ApiRest.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace ApiRest.Controllers
+{
+    public class TareaController : ApiController
+    {
+        // GET: Tarea
+        public IEnumerable<TAREA> Get()
+        {
+            Service1Client client = new ConexionConWCF().ConexionWCF();
+            var lista = client.ObtenerTareas();
+            return lista;
+        }
+
+        // GET: Tarea/Details/5
+
+        // POST: Tarea/Create
+        public void Post([FromBody]Tarea tarea)
+        {
+            try
+            {
+                Service1Client client = new ConexionConWCF().ConexionWCF();
+                TAREA t = new TAREA();
+                t.ID = tarea.ID;
+                t.TITULO = tarea.TITULO;
+                t.NOTAS = tarea.NOTAS;
+                t.ESTADO = tarea.ESTADO;
+                t.PRIORIDAD = tarea.PRIORIDAD;
+                t.FECHA_CREACION = tarea.FECHA_CREACION;
+                t.FECHA_TERMINO = tarea.FECHA_TERMINO;
+                client.InsertarTarea(t);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+            
+        }
+
+        // PUT: Tarea/Edit/5
+        public void Put([FromBody] Tarea tarea)
+        {
+            try
+            {
+                Service1Client client = new ConexionConWCF().ConexionWCF();
+                TAREA t = new TAREA();
+                t.ID = tarea.ID;
+                t.TITULO = tarea.TITULO;
+                t.NOTAS = tarea.NOTAS;
+                t.ESTADO = tarea.ESTADO;
+                t.PRIORIDAD = tarea.PRIORIDAD;
+                t.FECHA_CREACION = tarea.FECHA_CREACION;
+                t.FECHA_TERMINO = tarea.FECHA_TERMINO;
+                client.EditarTarea(t);
+
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+
+        // DELETE: Tarea/Delete/5
+        public void Delete(int id)
+        {
+            try
+            {
+                Service1Client client = new ConexionConWCF().ConexionWCF();
+                client.EliminarTarea(id);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# ApiRest 💻
+## Description 📚
+ En esta api se podra hacer un crud y esta conectada con un servicio WCF 
+## Installation 💡 
+ TENDRAS QUE MODIFICAR TANTO COMO EN EL PROYECTO WCF Y EL PROYECTO MVC LOS ARCHIVOS "WEB.CONFIG".
+### EN MI CASO LO ENCONTRARAS ASI:
+-"address="http://localhost/Service1.svc/mex"
+### TIENES QUE EDITARLO DE ESTA FORMA:
+ -"address="http://localhost:TUPUERTODESALIDA/Service1.svc/mex" 
+## App features 🪐
+ - LISTAR TAREAS
+- EDITAR TAREAS
+- ELIMINAR TAREAS
+- CREAR TAREAS 
+## Frontend Development 🌞 
+ ![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white) ![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white) ![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E) ![Bootstrap](https://img.shields.io/badge/bootstrap-%23563D7C.svg?style=for-the-badge&logo=bootstrap&logoColor=white) 
+## Backend Development 🌚 
+ ![C#](https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=c-sharp&logoColor=white) 
+ 
